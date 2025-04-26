aa53763fc7fafcce428b45adb0f6888fc68b8e66
Brynbing03 <bin24005@byui.edu>
2025-04-26 09:36:25 -0600

    I completed the C# part 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
-
+using System.Runtime.CompilerServices;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is you grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >=70)
+        {
+            Console.Write("You passed the class! Great job!");
+        }
+        else
+        {
+            Console.Write("Sorry, you did not pass. Try again next time. You got this!");
+        }
     }
 }

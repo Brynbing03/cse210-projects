b93e448d419c6a76d33f6f00391bc656fe4a224a
Brynbing03 <bin24005@byui.edu>
2025-04-29 14:54:38 -0600

    Did TEAM SQEAKY CHEESE PRACTICE on sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,28 @@
-using System;
+// using System;
 
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Sandbox World!");
+//     }
+// }
+
+
+// TEAM SQEAKY CHEESE PRACTICE! :)
+// April 29,2025
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine($"Today, I went to the zoo and saw a");
+
+        Console.Write("Type an adjective: ");
+        string adjective = Console.ReadLine();
+        
+        Console.Write("Choose an animal: ");
+        string animal = Console.ReadLine();
+
+        Console.WriteLine($"Today, I went to the zoo and saw a {adjective} {animal} jumping up and down in its tree.");
     }
 }

d3b92d648ee67ec49f87c9c09344a50cdf40f7c2
Brynbing03 <bin24005@byui.edu>
2025-05-03 17:57:37 -0600

    I completed the C# part 3-5 assignments

---
+++ ClassNotes/Program.cs
@@ -0,0 +1,55 @@
+﻿// ## Instructions for creating a new project in this solution:
+
+// 1. Create a new folder: `mkdir unitX/MyProject`
+// 2. Create the project: `cd unitX/MyProject;dotnet new console;cd ../..`
+// 3. Add the project to the solution: `dotnet sln add ./unitX/MyProject/MyProject.csproj`
+
+
+// See https://aka.ms/new-console-template for more information
+// The line below is a test to make sure that everything is working correctly
+// Console.WriteLine("Hello, World!");
+
+// LISTS using C#
+// making a string list
+List<string> animals = new List<string>();
+// making a number list
+List<int> debits = new List<int>();
+
+// to add things to the list, do this
+animals.Add("Cow");
+animals.Add("Horse");
+animals.Add("Pig");
+
+// to loop through lists - make sure to state what kind of list you are making (exp.String)
+foreach(String animal in animals)
+{
+    Console.WriteLine(animal);
+}
+// we can also figure out the len(in python) or count of what you have
+Console.WriteLine(animals.Count);
+
+// this is an if else statement that it going through the list to define if there is Dog in the list or not
+if (animals.Contains("Dog"))
+{
+    Console.WriteLine("There is Dog");
+}
+else
+{
+    Console.WriteLine("There is not a Dog...");
+}
+
+
+
+
+// Python Function Defined
+// def add_numbers(a, b);
+//      sum = a + b
+//      return sum
+
+int AddNumbers(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+Console.WriteLine(AddNumbers(1, 2));

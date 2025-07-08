81757e807536f5e78c2b1835708dc2f9dce3e58d
Brynbing03 <bin24005@byui.edu>
2025-07-08 14:54:32 -0600

    foundation 1 abstraction program: youtube video finished :) i think

--- ClassNotes/Program.cs
+++ ClassNotes/Program.cs
@@ -1,73 +1,73 @@
-﻿// ## Instructions for creating a new project in this solution:
+﻿// // ## Instructions for creating a new project in this solution:
 
-// 1. Create a new folder: `mkdir unitX/MyProject`
-// 2. Create the project: `cd unitX/MyProject;dotnet new console;cd ../..`
-// 3. Add the project to the solution: `dotnet sln add ./unitX/MyProject/MyProject.csproj`
-
-
-// See https://aka.ms/new-console-template for more information
-// The line below is a test to make sure that everything is working correctly
-// Console.WriteLine("Hello, World!");
-
-// LISTS using C#
-// making a string list
-List<string> animals = new List<string>();
-// making a number list
-List<int> debits = new List<int>();
-
-// to add things to the list, do this
-animals.Add("Cow");
-animals.Add("Horse");
-animals.Add("Pig");
-
-// to loop through lists - make sure to state what kind of list you are making (exp.String)
-foreach(String animal in animals)
-{
-    Console.WriteLine(animal);
-}
-// we can also figure out the len(in python) or count of what you have
-Console.WriteLine(animals.Count);
-
-// this is an if else statement that it going through the list to define if there is Dog in the list or not
-if (animals.Contains("Dog"))
-{
-    Console.WriteLine("There is Dog");
-}
-else
-{
-    Console.WriteLine("There is not a Dog...");
-}
+// // 1. Create a new folder: `mkdir unitX/MyProject`
+// // 2. Create the project: `cd unitX/MyProject;dotnet new console;cd ../..`
+// // 3. Add the project to the solution: `dotnet sln add ./unitX/MyProject/MyProject.csproj`
 
 
+// // See https://aka.ms/new-console-template for more information
+// // The line below is a test to make sure that everything is working correctly
+// // Console.WriteLine("Hello, World!");
+
+// // LISTS using C#
+// // making a string list
+// List<string> animals = new List<string>();
+// // making a number list
+// List<int> debits = new List<int>();
+
+// // to add things to the list, do this
+// animals.Add("Cow");
+// animals.Add("Horse");
+// animals.Add("Pig");
+
+// // to loop through lists - make sure to state what kind of list you are making (exp.String)
+// foreach(String animal in animals)
+// {
+//     Console.WriteLine(animal);
+// }
+// // we can also figure out the len(in python) or count of what you have
+// Console.WriteLine(animals.Count);
+
+// // this is an if else statement that it going through the list to define if there is Dog in the list or not
+// if (animals.Contains("Dog"))
+// {
+//     Console.WriteLine("There is Dog");
+// }
+// else
+// {
+//     Console.WriteLine("There is not a Dog...");
+// }
 
 
-// Python Function Defined
-// def add_numbers(a, b);
-//      sum = a + b
-//      return sum
-
-int AddNumbers(int a, int b)
-{
-    int sum = a + b;
-    return sum;
-}
-
-Console.WriteLine(AddNumbers(1, 2));
 
 
-public class SecretFamilyRecipe
-{
-    public string _password = "GrandmaDespisesBroccoli";
+// // Python Function Defined
+// // def add_numbers(a, b);
+// //      sum = a + b
+// //      return sum
 
-    public string _recipe = "Quesadilla: Spread a thin layer of mayo on one tortilla, and shredded scheed and optional fillinfs, top with another tortilla, then cook in a skillet until crispy brown";
+// int AddNumbers(int a, int b)
+// {
+//     int sum = a + b;
+//     return sum;
+// }
 
-    public string Reveal(string password)
-    {
-        if (password == _password)
-            return _recipe;
-        else
-        {
-            return "Nope, you're wrong...";
-        }
-    }
-}
+// Console.WriteLine(AddNumbers(1, 2));
+
+
+// public class SecretFamilyRecipe
+// {
+//     public string _password = "GrandmaDespisesBroccoli";
+
+//     public string _recipe = "Quesadilla: Spread a thin layer of mayo on one tortilla, and shredded scheed and optional fillinfs, top with another tortilla, then cook in a skillet until crispy brown";
+
+//     public string Reveal(string password)
+//     {
+//         if (password == _password)
+//             return _recipe;
+//         else
+//         {
+//             return "Nope, you're wrong...";
+//         }
+//     }
+// }
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,110 @@
 using System;
+using System.Collections.Generic;
 
+// this models a comment on haha a fake video
+class Comment
+{
+    public string CommenterName { get; }
+    public string CommentText { get; }
+
+    public Comment(string name, string text)
+    {
+        CommenterName = name;
+        CommentText = text;
+    }
+}
+
+// this represents a youtube video and stores its bumpin comments 
+class Video
+{
+    public string Title { get; }
+    public string Author { get; }
+    public int Length { get; }
+    private List<Comment> comments;
+
+    // the constructors
+    // creates an empty list to store the comments for a video
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        comments = new List<Comment>();
+    }
+
+    // this takes a comment object and adds it to the internal comments list
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    // this returns the total number of comments on the said video
+    public int GetNumberOfComments()
+    {
+        return comments.Count;
+    }
+
+    // this returns the actual list of comments 
+    // this allows other code to loop through them
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+
+    // this prints all the video stuff like the title, author, length and how many comments for each video
+    // it then loops through each comment and prints the name of the person who left a comment and what that comment was
+    // I added some blank lines too for visual spacing
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+
+        foreach (Comment c in comments)
+        {
+            Console.WriteLine($"- {c.CommenterName}: {c.CommentText}");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+// this is the main program! this is where it all starts...
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // I created a list that stores all my pretty good video objects
+        List<Video> videos = new List<Video>();
+
+        // haha I am kind of pround of these fake youtube videos and comments!!!! Bahahahaha! I hope you love them! 
+
+        Video v1 = new Video("Being a toddler for a day!", "Nugget Nathan", 342);
+        v1.AddComment(new Comment("Mr. Snooze", "I tried it and I peaked at nap time! zzzzzz"));
+        v1.AddComment(new Comment("Bitey", "I loved this 100%."));
+        v1.AddComment(new Comment("Sticky Ricky", "I vibe with this so much... "));
+        v1.AddComment(new Comment("Boogie Oogie", "I do this everyday!"));
+        videos.Add(v1);
+
+        Video v2 = new Video("How to swim like a mermaid!", "Oceanna", 726);
+        v2.AddComment(new Comment("Finley", "My new favorite thing!"));
+        v2.AddComment(new Comment("Pearl", "Can't wait to try this literally tonight in the hot tub."));
+        v2.AddComment(new Comment("Waverly", "Just go with the waves, truely."));
+        videos.Add(v2);
+
+        Video v3 = new Video("Funny baby FAILS", "Parent Life", 478);
+        v3.AddComment(new Comment("Kimmy", "#MiniMeltdown2.0"));
+        v3.AddComment(new Comment("Earl", "Baby vs. Gravity and... GRAVITY WINS."));
+        v3.AddComment(new Comment("Andrew", "Oops, I pooped again... diaper disaster!"));
+        videos.Add(v3);
+
+        // this displays all of the videos
+        // it loops through the video list
+        // then it prints all the videos details and that videos comments
+        foreach (Video v in videos)
+        {
+            v.DisplayVideoInfo();
+        }
     }
 }
